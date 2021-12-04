f5f2d1eed52a41b7ddfe8a65a981a6a0bbf1106e
Javier Beltrán <54746606+javy10@users.noreply.github.com>
2021-12-04 10:19:10 -0600

    arreglando el footer (#9)

--- packages/landing-gatsby/src/containers/AppMinimal/CallToAction/index.js
+++ packages/landing-gatsby/src/containers/AppMinimal/CallToAction/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { useStaticQuery, graphql, Link } from 'gatsby';
-import { Icon } from 'react-icons-kit';
-import { androidArrowForward } from 'react-icons-kit/ionicons/androidArrowForward';
-import Container from 'common/components/UI/Container';
-import Heading from 'common/components/Heading';
-import Text from 'common/components/Text';
-import CallToActionArea, { Left, Menu,
+import React from "react";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import { Icon } from "react-icons-kit";
+import { androidArrowForward } from "react-icons-kit/ionicons/androidArrowForward";
+import Container from "common/components/UI/Container";
+import Heading from "common/components/Heading";
+import Text from "common/components/Text";
+import CallToActionArea, {
+  Left,
+  Menu,
   CopyText,
   Social,
-  SocialText } from './callToAction.style';
-import logo from '../../../common/assets/image/app-minimal/footer/logo.png'
-import powerby from '../../../common/assets/image/app-minimal/footer/power.png'
-import face from '../../../common/assets/image/app-minimal/footer/face.png'
-import insta from '../../../common/assets/image/app-minimal/footer/insta.png'
+  SocialText,
+} from "./callToAction.style";
+import logo from "../../../common/assets/image/app-minimal/footer/logo.png";
+import powerby from "../../../common/assets/image/app-minimal/footer/power.png";
+import face from "../../../common/assets/image/app-minimal/footer/face.png";
+import insta from "../../../common/assets/image/app-minimal/footer/insta.png";
 
 const CallToAction = () => {
   const Data = useStaticQuery(graphql`
@@ -34,31 +37,55 @@ const CallToAction = () => {
   return (
     <CallToActionArea>
       <Container className="Container">
-      <Left>
-      <img className="logofooter" src={logo}/>
-      <img className="power" src={powerby}/>
-      <br/>
-          <p className="centerface"><img className="facebook" src={face}/></p>
-          <a className="instagram"><img className="instagram" src={insta}/></a>
-          <br/>
-          <button  className="button">
-                  HABLAR POR WHATSAPP
-          </button>
-      </Left>
+        <div>
+          <Left>
+            <img className="logofooter" src={logo} />
+            <img className="power" src={powerby} />
+            <br />
+            <p className="centerface">
+              <img className="facebook" src={face} />
+            </p>
+            <a className="instagram">
+              <img className="instagram" src={insta} />
+            </a>
+            <br />
+            <button className="button">HABLAR POR WHATSAPP</button>
+          </Left>
+        </div>
         <Menu>
-        <h3 className="acercade">Acerca de Norttech</h3>
-        <p className="texto" >En Norttech Labs construimos productos y servicios que ayudan a las personas a estar listas para la infraestructura financiera del futuro.
-        NORTTECH LABS LTDA con su producto TIANKII PAY es un proveedor de servicios BITCOIN registrado en el Banco Central de Reserva de El Salvador BCR.
-        </p>
-        <a link="https://www.norttech.com/" className="link">www.norttech.com</a>
+          <h3 className="acercade">Acerca de Norttech</h3>
+          <p className="texto">
+            En Norttech Labs construimos productos y servicios que ayudan a las
+            personas a estar listas para la infraestructura financiera del
+            futuro. <br></br><br></br> NORTTECH LABS LTDA con su producto TIANKII PAY es un
+            proveedor de servicios BITCOIN registrado en el Banco Central de
+            Reserva de El Salvador BCR.
+            <a link="https://www.norttech.com/" className="link">
+              www.norttech.com
+            </a>
+          </p>
         </Menu>
         <Social>
-        <h3 className="acercade">Acerca de Norttech</h3>
-        <p className="texto" >En Norttech Labs construimos productos y servicios que ayudan a las personas a estar listas para la infraestructura financiera del futuro.
-        NORTTECH LABS LTDA con su producto TIANKII PAY es un proveedor de servicios BITCOIN registrado en el Banco Central de Reserva de El Salvador BCR.
-        </p>
-        <a link="https://www.norttech.com/" className="link">www.norttech.com</a>
-          
+          <div>
+            <div>
+              <h3 className="support">Support & Contact</h3>
+              <p className="textoS">
+                Mail: <p className="textoD">support@norttech.com</p> <br></br>
+                Portal: <p className="textoD">support@norttech.com</p> <br></br>
+                Contact us: <p className="textoD">info@norttech.com</p>
+              </p>
+            </div>
+            <div>
+              <h3 className="product">Products</h3>
+              <p className="textoP">
+                Tiankii Pay <br></br>
+                Tiankii Chivo Wallet Connector <br></br>
+                Tiankii-LNURL Connector <br></br>
+                Tiankii Wallet <br></br>
+                Tiankii Market
+              </p>
+            </div>
+          </div>
         </Social>
         {/* <Left>
         <div>
@@ -74,15 +101,13 @@ const CallToAction = () => {
         </div>
         </Left>
         <Right>
-        <div>
-        <h3 className="acercade">Acerca de Norttech</h3>
-        <p className="texto" >En Norttech Labs construimos productos y servicios que ayudan a las personas a estar listas para la infraestructura financiera del futuro.
-        NORTTECH LABS LTDA con su producto TIANKII PAY es un proveedor de servicios BITCOIN registrado en el Banco Central de Reserva de El Salvador BCR.
-        </p>
-        <a link="https://www.norttech.com/" className="link">www.norttech.com</a>
-        
-        
-        </div>
+          <div>
+          <h3 className="acercade">Acerca de Norttech</h3>
+          <p className="texto" >En Norttech Labs construimos productos y servicios que ayudan a las personas a estar listas para la infraestructura financiera del futuro.
+          NORTTECH LABS LTDA con su producto TIANKII PAY es un proveedor de servicios BITCOIN registrado en el Banco Central de Reserva de El Salvador BCR.
+          </p>
+          <a link="https://www.norttech.com/" className="link">www.norttech.com</a>
+          </div>
         </Right> */}
       </Container>
     </CallToActionArea>
--- packages/landing-gatsby/src/containers/AppMinimal/SecureTransaction/index.js
+++ packages/landing-gatsby/src/containers/AppMinimal/SecureTransaction/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useStaticQuery, graphql, Link } from 'gatsby';
-import Text from 'common/components/Text';
-import Heading from 'common/components/Heading';
-import { Icon } from 'react-icons-kit';
-import { androidArrowForward } from 'react-icons-kit/ionicons/androidArrowForward';
-import Image from 'common/components/Image';
-import Container from 'common/components/UI/Container';
-import GatsbyImage from 'common/components/GatsbyImage';
-import SectionWrapper, { ContentWrapper } from './secure-transaction.style';
-import tiankii from '../../../common/assets/image/app-minimal/tiankii.png';
-import icono from '../../../common/assets/image/app-minimal/icono.png';
+import React from "react";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import Text from "common/components/Text";
+import Heading from "common/components/Heading";
+import { Icon } from "react-icons-kit";
+import { androidArrowForward } from "react-icons-kit/ionicons/androidArrowForward";
+import Image from "common/components/Image";
+import Container from "common/components/UI/Container";
+import GatsbyImage from "common/components/GatsbyImage";
+import SectionWrapper, { ContentWrapper } from "./secure-transaction.style";
+import tiankii from "../../../common/assets/image/app-minimal/tiankii.png";
+import icono from "../../../common/assets/image/app-minimal/icono.png";
 
 const SecureTransaction = () => {
   const Data = useStaticQuery(graphql`
@@ -41,12 +41,15 @@ const SecureTransaction = () => {
   return (
     <SectionWrapper>
       <Container>
-
         <div>
-          <img className="imageTiankii" src={tiankii}/>
+          <img className="imageTiankii" src={tiankii} />
         </div>
-        <h3 className="pasarela" >Pasarela de pagos Bitcoin</h3>
-        <span className="vender">Vender por Redes Sociales con Bitcoin nunca había sido tan fácil!, Cree Facturas, Enlaces o botones de pago y envíe a sus clientes estos enlaces por Messenger, email, Whatsapp, etc</span>
+        <h3 className="pasarela">Pasarela de pagos Bitcoin</h3>
+        <span className="vender">
+          Vender por Redes Sociales con Bitcoin nunca había sido tan fácil!,
+          Cree Facturas, Enlaces o botones de pago y envíe a sus clientes estos
+          enlaces por Messenger, email, Whatsapp, etc
+        </span>
 
         <ContentWrapper>
           <div className="image">
@@ -58,23 +61,21 @@ const SecureTransaction = () => {
               }
               alt="Transaction"
             />
-            
           </div>
           <div className="content">
-          <div className="text1">
-          <Heading  content={title} />
-            <Text content={description} />
+            <div className="text1">
+              <Heading content={title} />
+              <Text content={description} />
+            </div>
+            <div className="text1">
+              <Heading content={title} />
+              <Text content={description} />
+            </div>
+            <div className="text1">
+              <Heading content={title} />
+              <Text content={description} />
+            </div>
           </div>
-          <div className="text1">
-          <Heading  content={title} />
-            <Text content={description} />
-          </div>
-          <div className="text1">
-          <Heading  content={title} />
-            <Text content={description} />
-          </div>
-          </div>
-          
         </ContentWrapper>
       </Container>
     </SectionWrapper>
